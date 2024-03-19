8a9645694b276832d2017f8d1108e99b36ce4be6
Francis Mutiso <franckmutiso@gmail.com>
2024-03-19 11:19:29 +0300

    initial commit

---
+++ src/main/java/com/generics/genericbankingapi/GenericBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.generics.genericbankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericBankingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericBankingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/generics/genericbankingapi/GenericBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generics.genericbankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericBankingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
